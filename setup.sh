0240dfb76f832d1be61302df04936519f76b3df8
Yoshiya Miyata <ymiyata3@gatech.edu>
2012-07-02 20:31:45 -0400

    Updated setup script to check whether the swap and backup directory already exists or not before creating one

--- setup.sh
+++ setup.sh
@@ -2,6 +2,17 @@
 
 git submodule init
 git submodule update
-mkdir .vimswap .vimbackup
 
+if [ ! -d ~/.vimswap ]; then
+    mkdir ~/.vimswap
+    echo "Created directory ~/.vimswap. This directory will contain all the vim swap files"
+else
+    echo "~/.vimswap directory already exists. Skipping this step"
+fi
 
+if [ ! -d ~/.vimbackup ]; then
+    mkdir ~/.vimbackup
+    echo "Created directory ~/.vimbackup. This directory will contain all the vim backup files"
+else
+    echo "~/.vimbackup directory already exists. Skipping this step"
+fi
