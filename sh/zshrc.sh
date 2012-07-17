27ebbefae5718220eaa44c943ce5d437309f9134
Yoshiya Miyata <ymiyata3@gmail.com>
2012-07-17 02:06:08 -0400

    Added suffix aliases

--- sh/zshrc.sh
+++ sh/zshrc.sh
@@ -20,7 +20,33 @@ setopt extended_history
 setopt inc_append_history
 setopt extended_glob # More powerful globbing
 setopt auto_cd
+unsetopt correct
+unsetopt correct_all
 
 HISTSIZE=SAVEHIST=10000
 REPORTTIME=10
 
+###############################################################
+# => zsh suffix aliases 
+###############################################################
+alias -s c=vim
+alias -s cc=vim
+alias -s cpp=vim
+alias -s coffee=vim
+alias -s html=vim
+alias -s java=vim
+alias -s js=vim
+alias -s m=vim
+alias -s markdown=vim
+alias -s md=vim
+alias -s pl=vim
+alias -s py=vim
+alias -s scala=vim
+alias -s sh=vim
+alias -s rb=vim
+alias -s tex=vim
+alias -s txt=vim
+
+alias -s gz="tar -xvzf"
+alias -s tar="tar -xvf"
+alias -s zip=unzip
