23e8af3a064fd1969aded3c9d626d0cabfcadf78
Yoshiya Miyata <ymiyata3@gmail.com>
2012-07-17 01:33:59 -0400

    Added zsh configurations, and added few common configurations

--- sh/zshrc.sh
+++ sh/zshrc.sh
@@ -1,10 +1,26 @@
 ###############################################################
 # => oh-my-zsh configuration
 ###############################################################
-###############################################################
-# => zsh specific configuration 
-###############################################################
 export DISABLE_AUTO_UPDATE="true"
 export ZSH="$HOME/.oh-my-zsh"
 export ZSH_THEME="kennethreitz"
 source $ZSH/oh-my-zsh.sh
+
+###############################################################
+# => zsh specific configuration 
+###############################################################
+autoload -U compinit
+compinit
+
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+
+setopt complete_in_word
+setopt share_history
+setopt extended_history
+setopt inc_append_history
+setopt extended_glob # More powerful globbing
+setopt auto_cd
+
+HISTSIZE=SAVEHIST=10000
+REPORTTIME=10
+
